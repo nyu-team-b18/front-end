66fbc55324223f61e7702a0f8447035a1588d400
Justin Rivera <50186921+jriv01@users.noreply.github.com>
2023-04-24 10:19:44 -0400

    Added more endpoints

--- app.py
+++ app.py
@@ -61,6 +61,60 @@ def getStudents():
         'students': students,
     })
 
+@app.route('/getAccountType', methods=["GET"])
+def getAccountType():
+    # Get account type
+    # TODO: Check session for account type
+
+    account_type = 'ADMIN'
+
+    return jsonify({'account-type':account_type})
+
+@app.route('/getAssignmentDetails', methods=["GET"])
+def getAssignmentDetails():
+    # GET args
+    assignment_title = request.args.get('assignment')
+    
+    # Get assignment description and date
+    # TODO: Add a query for getting the assignment details
+    assignment_description = "Just an assignment, nothing to see here"
+    assignment_date = "2023-05-04"
+    return jsonify({
+        'title': assignment_title,
+        'description': assignment_description,
+        'date': assignment_date
+    })
+
+@app.route('/updateAssignment', methods=["GET", "POST"])
+def updateAssignment():
+    if(request.method == 'POST'):
+        data = request.get_json()
+        title, description, date = data['title'], data['description'], data['date']
+
+        # Update the assignment
+        # TODO: Add query to update database at assignment title
+
+    return jsonify({})
+
+
+@app.route('/createAssignment', methods=["GET", "POST"])
+def createAssignment():
+    data = request.get_json()
+    title, description, date = data['title'], data['description'], data['date']
+
+    # Check if the assignment title already exists
+    check = True
+
+    # Create the assignment
+    # TODO: Add query to create new assignment
+    if check:   # Success
+        # TODO: Compile relevant information into dictionary
+        return jsonify({}), 200
+    else:       # Failure
+        # TODO: Compile relevant information into dictionary
+        return jsonify({}), 409
+
+
 app.secret_key = 'some other random key'
 
 
